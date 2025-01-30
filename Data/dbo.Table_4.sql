4cccf96fe2d4070a5bba9f8007ffd0c70619e56a
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-01-30 15:48:39 +0600

    Added all the sql tables

---
+++ Data/dbo.Table_4.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[StockTransaction] (
+    [TransactionID]   INT             IDENTITY (1, 1) NOT NULL,
+    [ProductID]       INT             NOT NULL,
+    [TransactionType] VARCHAR (50)    NOT NULL,
+    [Quantity]        INT             NOT NULL,
+    [TransactionDate] DATETIME        NOT NULL,
+    [ReferenceID]     INT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([TransactionID] ASC),
+    CONSTRAINT [FK_StockTransaction_Product] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID]) ON DELETE CASCADE ON UPDATE CASCADE,
+    CHECK ([TransactionType]='Purchase' OR [TransactionType]='Sale' OR [TransactionType]='Adjustment')
+);
